fd6ec623ff1442c36f1ed9d905cb8e172c2aee40
NicolaM94 <nicola.moro2312@gmail.com>
2024-11-15 15:11:25 +0100

    refactor

--- funcs/fibrec.c
+++
@@ -1,7 +0,0 @@
-/*Calcola il valore della serie fibonacci alla posizione pos*/
-int FibonacciRicorsivo (int pos) {
-    if (pos <= 1) {
-        return 1;
-    }
-    return FibonacciRicorsivo(pos-1) + FibonacciRicorsivo(pos-2);
-}
